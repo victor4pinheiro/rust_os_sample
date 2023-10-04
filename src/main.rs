827477a3991a9a2dd4bf1152d2db20c5f0c717fd
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-10-04 17:07:42 -0300

    feat: add static text to VGA text mode

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
     loop {}
 }
